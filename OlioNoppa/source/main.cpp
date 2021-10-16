c336654fe0f1024af2cf5a2da73ab39030f16353
TuomasKarjalainen <91312571+TuomasKarjalainen@users.noreply.github.com>
2021-10-16 13:44:09 +0300

    Update main.cpp

--- OlioNoppa/source/main.cpp
+++ OlioNoppa/source/main.cpp
@@ -6,7 +6,7 @@ int main(int argc, const char** argv) {
 
 	setlocale(LC_ALL, "Finnish");
 
-	olionoppa n("~ A M E R I C A N  D I C E 2.0 ~", "Tatu, Tuomas, Lauri", "Pelivoitot.dat");
+	olionoppa n("~ A M E R I C A N  D I C E 2.0 ~", "Tuomas", "Pelivoitot.dat");
 	n.aloitus();
 		
 }
